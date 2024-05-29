98b64027dd89d19bba0450e19e9a0e6e08f05756
bashar <basharhasan74@gmail.com>
2024-05-29 20:23:18 +0300

    refactor(Composite): Replace Contact with more abstracted type PhoneContact

--- src/Composite/PhoneContact/Main.java
+++ src/Composite/PhoneContact/Main.java
@@ -2,8 +2,8 @@ package Composite.PhoneContact;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        Contact firstContact = new Contact("firstContact", "0912321");
-        Contact secondContact = new Contact("secondContact", "091232212");
+        PhoneContact firstContact = new Contact("firstContact", "0912321");
+        PhoneContact secondContact = new Contact("secondContact", "091232212");
         Group firstGroup = new Group("Group");
         firstGroup.add(firstContact);
         firstGroup.add(secondContact);
